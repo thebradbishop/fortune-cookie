d25d4571920a865064964875b17190f6f0f80977
Brad Bishop <brad.bishop@me.com>
2017-01-22 20:24:49 -0600

    created fortune cookie webapp

--- main.py
+++ main.py
@@ -15,11 +15,35 @@
 # limitations under the License.
 #
 import webapp2
+import random
+
+def get_random_fortune():
+    fortunes = [
+        "I see much code in your future.",
+        "Consider eating more fortune cookies.",
+        "You have tamed the mighty Python, now you must free it onto the Great Spider's web!"
+    ]
+    index = random.randint(0,2)
+    return fortunes[index]
 
 class MainHandler(webapp2.RequestHandler):
     def get(self):
-        self.response.write('Hello world!')
+        header = "<h1>Fortune Cookie</h1>"
+        fortune = "<strong>" + get_random_fortune() + "</strong>"
+        fortune_sentence = "Your fortune: " + fortune
+        fortune_paragraph = "<p>" + fortune_sentence + "</p>"
+        luckyNumber = "<strong>" + str(random.randint(1, 100)) + "</strong>"
+        number_sentence = "Your Lucky Number: " + luckyNumber
+        number_paragraph = "<p>" + number_sentence + "</p>"
 
-app = webapp2.WSGIApplication([
-    ('/', MainHandler)
-], debug=True)
+        cookie_again_button = "<a href='.'><button>Another Cookie Please!</button></a>"
+
+        content = header + fortune_paragraph + number_paragraph + cookie_again_button
+
+        self.response.write(content)
+
+routes = [
+    ('/', MainHandler),
+]
+
+app = webapp2.WSGIApplication(routes, debug=True)
